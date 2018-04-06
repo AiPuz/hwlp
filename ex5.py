8634133e206fcc962d3281d558a8a0141d6fc82c
tobecoder <tobecoder@outlook.com>
2018-04-06 10:44:03 -0700

    [hwlp] ex5:format input

---
+++ ex5.py
@@ -0,0 +1,27 @@
+# -- coding = utf-8 --
+
+my_name = 'zed a shaw'
+
+my_age = 35
+
+my_height = 180
+
+my_weight = 180
+
+my_eyes = 'blue'
+
+my_teeth = 'white'
+
+my_hair = 'brown'
+
+print "lets talk about %s" % my_name
+
+print "his height is %d " % my_height
+
+print "his weight is %d" % my_weight
+
+print "he's got %s eyes and %s hair" %(my_eyes, my_hair)
+
+print "his teeth are %s depending on the coffee" % my_teeth
+
+print "if i add %d, %d, and %d i get %d." % (my_age, my_height, my_weight, my_age+my_height+my_weight)
