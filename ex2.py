54e064e331608787411a106c6202cd87bca16e1c
tobecoder <tobecoder@outlook.com>
2018-04-06 07:32:19 -0700

    [hwlp]ex2 and ex3

---
+++ ex2.py
@@ -0,0 +1,12 @@
+# a comment, this is so you can read your program later
+
+# anything after the # is ignored by python
+
+print "I could have code like this " # and the comment after is ignored 
+
+# You can alse use a comment disable or comment out a piece of code:
+
+
+# print "this won't run"
+
+print "this will run"
