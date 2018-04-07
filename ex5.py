f0443e7ce41aaeec5e429d62b463ffda3a43a486
tobecoder <tobecoder@outlook.com>
2018-04-07 01:08:29 -0700

    [hwlp] ex5 and ex6:print and string

--- ex5.py
+++ ex5.py
@@ -1,27 +1,27 @@
 # -- coding = utf-8 --
 
-my_name = 'zed a shaw'
+name = 'zed a shaw'
 
-my_age = 35
+age = 35
 
-my_height = 180
+height = 180
 
-my_weight = 180
+weight = 180
 
-my_eyes = 'blue'
+eyes = 'blue'
 
-my_teeth = 'white'
+teeth = 'white'
 
-my_hair = 'brown'
+hair = 'brown'
 
-print "lets talk about %s" % my_name
+print "lets talk about %s" % name
 
-print "his height is %d " % my_height
+print "his height is %d " % height
 
-print "his weight is %d" % my_weight
+print "his weight is %d" % weight
 
-print "he's got %s eyes and %s hair" %(my_eyes, my_hair)
+print "he's got {} eyes and {} hair".format(eyes, hair)
 
-print "his teeth are %s depending on the coffee" % my_teeth
+print "his teeth are %s depending on the coffee" % teeth
 
-print "if i add %d, %d, and %d i get %d." % (my_age, my_height, my_weight, my_age+my_height+my_weight)
+print "if i add %d, %d, and %d i get %d." % (age, height, weight, age+height+weight)
